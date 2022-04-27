55320cce265ace1b0d3f5a22d84e62b9bee256de
DESKTOP-RNAD6CU\trostel <trostel.connor@gmail.com>
2022-04-26 21:47:42 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# B&R Industrial Automation Theme
+
+This is a base theme for use with MkDocs in the style of B&R Industrial Automation's documentation. You can see the style in action with this documentation. 
+
+!!! notice danger "CSS and HTML Incompatibilities"
+    It should be noted, that much of the style requirements are such to be able to support exporting for the AS Help as well as for standard webbrowsers. Because of this, many CSS or HTML elements may not be supported
+
+
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+VERSION = '1.0'
+
+
+setup(
+    name="mkdocs-br-industrial-theme",
+    version=VERSION,
+    url='https://github.com/mkdocs/mkdocs-basic-theme',
+    license='BSD',#?????
+    description='Theme to match with the standard B&R Industrial Automation documentation styling.',
+    author='Connor Trostel',
+    author_email='connor.trostel@br-automation.com',
+    packages=find_packages(),
+    include_package_data=True,
+    entry_points={
+        'mkdocs.themes': [
+            'brtheme = br_theme',
+        ]
+    },
+    zip_safe=False
+)
